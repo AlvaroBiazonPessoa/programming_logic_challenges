36d33c00c48da2d3119a3871441aa27f04790481
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2024-03-08 08:30:14 -0300

    feat: Adding class that contains resolution of challenge 1006

---
+++ src/Challenge1006.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Challenge1006 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double gradeA = scanner.nextDouble();
+        double gradeB = scanner.nextDouble();
+        double gradeC = scanner.nextDouble();
+        double weightOfGradeA = 2.0;
+        double weightOfGradeB = 3.0;
+        double weightOfGradeC = 5.0;
+        double average = ((gradeA * weightOfGradeA) + (gradeB * weightOfGradeB) + (gradeC * weightOfGradeC)) / ((weightOfGradeA + weightOfGradeB + weightOfGradeC));
+        System.out.printf("MEDIA = %.1f\n", average);
+        scanner.close();
+    }
+
+}
