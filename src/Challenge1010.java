87f4dedcbfa77db6e30822902bb908face128b27
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2024-03-11 14:03:55 -0300

    feat: Adding class that contains resolution of challenge 1010

---
+++ src/Challenge1010.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Challenge1010 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstPieceCode = scanner.nextInt();
+        int firstPieceQuantity = scanner.nextInt();
+        double firstPieceValue = scanner.nextDouble();
+        int secondPieceCode = scanner.nextInt();
+        int secondPieceQuantity = scanner.nextInt();
+        double secondPieceValue = scanner.nextDouble();
+        double amountToPay = firstPieceQuantity * firstPieceValue + secondPieceQuantity * secondPieceValue;
+        System.out.printf("Amount to pay: $ %.2f\n", amountToPay);
+    }
+
+}
